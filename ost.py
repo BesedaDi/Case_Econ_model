fdbd9e3a9b1e47ed25ba7b342f52e05c8a98a63d
BesedaDi <dianabesedina223@gmail.com>
2020-05-13 19:56:24 +0700

    start

---
+++ ost.py
@@ -0,0 +1,5 @@
+# Case-study #7 "economic model"
+# Developers: Besedina D. (%)
+#             Setskov M. (%)
+#             Moiseenko V. (%)
+# The aim: program that automates hotel room occupancy management.
